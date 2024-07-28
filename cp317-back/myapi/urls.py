6ff90622b8e2106875cd9424be419547378127e5
Chris M <math9620@mylaurier.ca>
2024-07-28 15:27:29 -0400

    /

--- cp317-back/myapi/urls.py
+++ cp317-back/myapi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('friends/', views.friends, name="friends"),
     path('group/', views.group, name="group"),
     path('signup/', views.signup, name="signup"),
-    path('time', views.current_time, name="timywimey")
+    path('time/', views.current_time, name="timywimey")
 
 
 ]

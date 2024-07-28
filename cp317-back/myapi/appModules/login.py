2986e5dc87c6210c900b8a271f117beca4e94350
Chris M <math9620@mylaurier.ca>
2024-07-28 13:49:49 -0400

    updated login system

--- cp317-back/myapi/appModules/login.py
+++ cp317-back/myapi/appModules/login.py
@@ -1,8 +1,10 @@
-#meow\
+# meow\
 from rest_framework.response import Response
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from bcrypt import hashpw, gensalt
+import bcrypt
 
 
 def loginreqs(request):
@@ -10,13 +12,33 @@ def loginreqs(request):
     app = firebase_admin.initialize_app(cred)
     db = firestore.client()
     users_ref = db.collection('creds')
+    if request.method == 'POST':
+        email = request.data['email']
+        password = request.data['password']
+        user = users_ref.where('email', '==', email).get()
+        if bcrypt.checkpw(password, user[0].to_dict()['password']):
+            return Response({'message': "Login Successful", 'id': user.id}, status=200)
+        else:
+            return Response({'message': "Login Failed"}, status=401)
 
-    return Response({'message': "test"})
+
 
 def signupreqs(request):
     cred = credentials.Certificate("cp317-back/serviceAccountKey.json")
     app = firebase_admin.initialize_app(cred)
     db = firestore.client()
     users_ref = db.collection('creds')
+    if request.method == 'POST':
+        email = request.data['email']
+        password = request.data['password']
+        user = users_ref.where('email', '==', email).get()
+        if user:
+            return Response({'message': "User already exists"}, status=400)
+        else:
+            user = users_ref.add({
+                'email': email,
+                'password': bcrypt.hashpw(password, bcrypt.gensalt())
+            })
+            return Response({'message': "User created", 'id': user.id}, status=201)
 
-    return Response({'message': "test"})
+

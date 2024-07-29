257e2084a0a4983c0fd55af3c5e0be5cb0b49f2d
Chris M <math9620@mylaurier.ca>
2024-07-29 10:34:55 -0400

    base inputs for back end server

--- cp317-back/myapi/appModules/friends.py
+++ cp317-back/myapi/appModules/friends.py
@@ -1,4 +1,7 @@
 from rest_framework.response import Response
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 def friends(request): #TODO implement
     return Response({'message': "test"})
--- cp317-back/myapi/appModules/group.py
+++ cp317-back/myapi/appModules/group.py
@@ -1,5 +1,7 @@
 from rest_framework.response import Response
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 def groups(request):  # TODO implement
     return Response({'message': 'meow'})

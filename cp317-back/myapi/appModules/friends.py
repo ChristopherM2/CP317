f61c086e0cb9a6403ffe3a70e6eca1fa790fff52
Chris M <math9620@mylaurier.ca>
2024-08-01 18:06:26 -0400

    undo thingy

--- cp317-back/myapi/appModules/friends.py
+++ cp317-back/myapi/appModules/friends.py
@@ -36,10 +36,10 @@ class friend:
                 else:
                     return Response({'message': "User does not exist"}, status=498)
                 user.update({
-                    'following': user.get('following').append(friend.get('settings').get('username'))
+                    'following': user.get('following').append(friend)
                 })
                 friend.update({
-                    'followers': friend.get('followers').append(user.get('settings').get('username'))
+                    'followers': friend.get('followers').append(user)
                 })
             except Exception as e:
                 return Response({'message': "Invalid request, missing fields :(((("}, status=418)
--- cp317-front/app/page.tsx
+++
@@ -1,9 +0,0 @@
-//import styles from "./globals.css"
-import HelloWorld from "./home/page"
-import TestComponent from "@/app/home/test-component";
-export default function Home() {
-  return (
-    <p>HelloWorld</p>
-  );
-}
-//meow

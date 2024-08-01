db4d7425e3f255d43c213c4d32782c4bcb868910
Chris M <math9620@mylaurier.ca>
2024-08-01 14:36:14 -0400

    fixed get thingy

--- cp317-back/myapi/views.py
+++ cp317-back/myapi/views.py
@@ -23,12 +23,12 @@ app = firebase_admin.initialize_app(cred)
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def hello_world(request):
     return Response({'message': 'if this works i wont end it all!!'}, status=200)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_user(request):
     return user.getuser(None,request, app)
 
@@ -49,7 +49,7 @@ def friends(request):
     return friends(None,request, app)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def new_group(request):
     return group.newgroup(None,request, app)
 
@@ -64,78 +64,78 @@ def remove_user_from_group(request):
     return group.removeuserfromGroup(None,request, app)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def get_group(request):
     return group.getgroup(None,request, app)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def current_time(request):
     now = dateformat.format(timezone.now(), 'Y-m-d H:i:s')
     return Response({'time': str(now)})
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def sendMessage(request):
     return group.sendmessage(None,request, app)
 
 
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def addTask(request):
     return group.addtask(None,request, app)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getTasks(request):
     return group.gettasks(None,request, app)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getMessages(request):
     return group.getmessages(None,request, app)
 
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def completetask(request):
     return group.completeTask(None,request, app)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getCompletedTasks(request):
     return group.getcompletedtasks(None,request, app)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getGroupMembers(request):
     return group.getgroupmembers(None,request, app)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateMemberCompletion(request):
     return group.updatemembercompletion(None,request, app)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateImage(request):
     return Settings.update_image(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateDarkmode(request):
     return Settings.update_darkmode(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateUsername(request):
     return Settings.update_username(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateTracking(request):
     return Settings.update_tracking(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updatePassword(request):
     return Settings.update_password(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def updateEmail(request):
     return Settings.update_email(None, request)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def getSettings(request):
     return Settings.get_settings(None, request)

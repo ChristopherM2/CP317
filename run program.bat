ee2c7e8e57f62dfb2a4d23283fdfa66634f0c642
Chris M <math9620@mylaurier.ca>
2024-08-04 13:50:31 -0400

    HOLY SHIT WHY DIDNT WE DO THIS SOONER

---
+++ run program.bat
@@ -0,0 +1,5 @@
+git pull origin main --force
+start powershell -NoExit -Command "pip3 install -r requirements.txt; cd cp317-back; python3 manage.py runserver"
+start powershell -NoExit -Command "cd cp317-front; npm install; npm run dev"
+timeout 5
+start http://localhost:3000

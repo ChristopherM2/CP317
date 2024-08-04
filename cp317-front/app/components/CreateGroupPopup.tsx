608b3d623214f861420d1107354bcb371b2be912
ShayanDhillon <Shayandhillon@gmail.com>
2024-08-04 14:25:17 -0400

    group popups for create/join made, functionality on its way

--- cp317-front/app/components/CreateGroupPopup.tsx
+++ cp317-front/app/components/CreateGroupPopup.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import styles from './styles/CreateGroupPopup.module.css'
+import styles from './styles/GroupPopup.module.css'
 
 interface PopupProps {
     onClose: () =>void;
@@ -22,14 +22,14 @@ const CreateGroupPopup:React.FC<PopupProps>  = ({onClose}) => {
 
   return (
     <div className={styles.popupOverlay}>
-            <div className={styles.popupContent}>
-                <h3 className={styles.title}>Create a Group</h3>
-                <input type="text" placeholder={'Enter a Group name'} onChange={(e) => setInput(e.target.value)} />
-                <button onClick={onClose} className={styles.closeButton} >×</button>
-                <button onClick={createGroup}>Create</button>
-                <p>{NameIssue}</p>
-            </div>
+        <div className={styles.popupContent}>
+            <h3 className={styles.title}>Create a Group</h3>
+            <input type="text" placeholder={'Enter a Group name'} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={onClose} className={styles.closeButton} >×</button>
+            <button onClick={createGroup}>Create</button>
+            <p>{NameIssue}</p>
         </div>
+    </div>
   )
 }
 
--- cp317-front/app/components/JoinGroupPopup.tsx
+++ cp317-front/app/components/JoinGroupPopup.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from './styles/GroupPopup.module.css'
 
 interface PopupProps{
     onClose: () => void;
 }
 
 const JoinGroupPopup: React.FC<PopupProps>= ({onClose}) => {
+    const [Input, setInput] = useState<string>('');
+    const [NameIssue, setNameIssue] = useState<string>('');
+
+
+    const joinGroup = async() =>{
+        console.log("join the group ");
+        try{
+            throw new Error("issue");
+
+        }catch(Error){
+            setNameIssue("That group doesnt exist, try another one");
+        }
+    }
+
   return (
-    <div>
-      
+    <div className={styles.popupOverlay}>
+        <div className={styles.popupContent}>
+            <h3 className={styles.title}>Join a Group</h3>
+            <input type="text" placeholder={'Enter a Group name'} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={onClose} className={styles.closeButton} >×</button>
+            <button onClick={joinGroup}>Create</button>
+            <p>{NameIssue}</p>
+            <div className={styles.list}>
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }

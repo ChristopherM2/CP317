557a978832bf8e0db4843570b99fef20d941d9b5
ShayanDhillon <Shayandhillon@gmail.com>
2024-08-04 14:49:49 -0400

    join groups ui half done, (should let you join a group)

--- cp317-front/app/components/JoinGroupPopup.tsx
+++ cp317-front/app/components/JoinGroupPopup.tsx
@@ -1,19 +1,55 @@
-import React, {useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import styles from './styles/GroupPopup.module.css'
+import AuthContext from './AuthContext';
 
 interface PopupProps{
     onClose: () => void;
 }
 
 const JoinGroupPopup: React.FC<PopupProps>= ({onClose}) => {
+    const Context = useContext(AuthContext);
     const [Input, setInput] = useState<string>('');
     const [NameIssue, setNameIssue] = useState<string>('');
+    const [AvailGroups, setAvailGroups] = useState<[]>([])
 
+
+    useEffect(() => {
+        const listGroups = async() => {
+            try {
+                const response = await fetch(`http://127.0.0.1:8000/api/getAvailableGroups/`,{method: 'GET'});
+
+                if(!response.ok) throw new Error("network response not ok")
+
+                const data = await response.json();
+                const {message} = data;
+                setAvailGroups(message);
+                console.log(message);
+
+            } catch(Error){
+                console.log("Error:" , Error);
+            }
+            
+        }
+        listGroups();
+    }, [])
+    
 
     const joinGroup = async() =>{
-        console.log("join the group ");
+        if(!Context?.user?.id) return;
         try{
-            throw new Error("issue");
+            const response = await fetch(`http://127.0.0.1:8000/api/addUserToGroup/`,
+                                        { method: 'POST',
+                                        headers: {
+                                          'Content-Type': 'application/json'
+                                        },
+                                        body: JSON.stringify({ token: Context.user.id, name:Input })
+                                      });
+            
+            if(!response.ok) throw new Error("network response was not ok");
+
+            const data = await response.json();
+            const {message} = data;
+            console.log("joined group")
 
         }catch(Error){
             setNameIssue("That group doesnt exist, try another one");
